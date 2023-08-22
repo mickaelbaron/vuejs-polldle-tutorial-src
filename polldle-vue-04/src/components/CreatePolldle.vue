1c66cc5d17ef40f68df53e4f5ac6bc59d8489362
Stéphen BOS <droup@pm.me>
2023-08-22 17:35:44 +0200

    Fix v-on : Replace '@:' by '@'
    Signed-off-by: Stéphen BOS <droup@pm.me>

--- polldle-vue-04/src/components/CreatePolldle.vue
+++ polldle-vue-04/src/components/CreatePolldle.vue
@@ -76,7 +76,7 @@ function createPolldle() {
           placeholder="Polldle Option"
           class="large-input mx-auto d-block"
           v-model="newPolldleOptionText"
-          @:keypress.enter="addPolldleOption"
+          @keypress.enter="addPolldleOption"
         />
       </div>
     </div>
@@ -87,7 +87,7 @@ function createPolldle() {
         <button
           type="button"
           class="clear-button btn-lg btn-danger mx-auto d-block"
-          @:click="clearAllPolldleOptions"
+          @click="clearAllPolldleOptions"
         >
           Clear all PollDLE Options
         </button>
@@ -107,7 +107,7 @@ function createPolldle() {
           type="button"
           class="validate-button btn-lg btn-primary mx-auto d-block"
           :disabled="isCreatePolldleDisabled()"
-          @:click="createPolldle"
+          @click="createPolldle"
         >
           Create PollDLE
         </button>
